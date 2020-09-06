b6d2e6f06d33f89bc0a08f6278e75da58e1129b2
Daniel Johannessen <daniel.johannessen@sesam.io>
2020-09-06 21:11:15 +0200

    adding function for string equality with test

--- calculator.py
+++ calculator.py
@@ -4,5 +4,8 @@ def add (x,y):
         max_decimal_length.append(len(repr(x).split('.')[-1]))
     if (type(y) == float):
         max_decimal_length.append(len(repr(y).split('.')[-1]))
+    if ((type(x) == str) and (type(y)==str)):
+        return x + y
+
     return round(x + y,max(max_decimal_length))
     
--- test_calculator.py
+++ test_calculator.py
@@ -5,3 +5,6 @@ def test_add_exercise_1():
 
 def test_add_exercise_2():
     assert calculator.add(0.1,0.2) == 0.3
+
+def test_add_exercise_3():
+    assert calculator.add("Hello ","World") == "Hello World"
