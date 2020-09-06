7672a035846b69c25f761883b1adc7c593436a8c
Daniel Johannessen <daniel.johannessen@sesam.io>
2020-09-06 21:06:30 +0200

    adding function for float equality with test

--- calculator.py
+++ calculator.py
@@ -1,2 +1,8 @@
 def add (x,y):
-    return x + y
+    max_decimal_length = [0]
+    if (type(x) == float):
+        max_decimal_length.append(len(repr(x).split('.')[-1]))
+    if (type(y) == float):
+        max_decimal_length.append(len(repr(y).split('.')[-1]))
+    return round(x + y,max(max_decimal_length))
+    
--- test_calculator.py
+++ test_calculator.py
@@ -2,3 +2,6 @@ import calculator
 
 def test_add_exercise_1():
     assert calculator.add(1,2) == 3
+
+def test_add_exercise_2():
+    assert calculator.add(0.1,0.2) == 0.3
