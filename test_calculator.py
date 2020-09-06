c3af8cd6eb73562fce37227ce289a7276b04cb94
Daniel Johannessen <daniel.johannessen@sesam.io>
2020-09-06 22:13:22 +0200

    Error tests added

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,6 @@
 import calculator
 import math
+import pytest
 
 def test_add_exercise_1():
     assert calculator.add(1,2) == 3
@@ -26,3 +27,11 @@ def test_multiplicate_exercise_4():
 
 def test_derivative_of_exponential_exercise_4():
     assert calculator.derivative_of_exponential(2) == 14.7781121978613
+
+def test_int_float_addition_with_string_raises_ValueError_exercise_5():
+    with pytest.raises(TypeError):
+        calculator.add("Hello",1)
+
+def test_zero_division_raises_ZeroDivisionError_exercise_5():
+    with pytest.raises(ZeroDivisionError):
+        calculator.divide(1,0)
